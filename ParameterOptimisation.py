Fixed Hessian Calculations
The Hessian values are now being calculated correctly. I still don't understand what the divergence error is which is effecting some of the results (these are currently being replaced with NaN's).

In addition to that error, I also need to write a section which will plot the results, which doesn't work right now since I don't have the Tkinter file which matplotlib needs.

ParameterOptimisation.py
@@ -200,28 +200,60 @@ def plotParameterEstimationError(x, y, dx, dy):
 ###########
 
 if __name__ == "__main__":
-    data = np.array(([[54.127, 0.724, -11.211], [54.127, 39.276, -11.211], [64.432, -0.669, 27.737], [64.432, 40.669, 27.737]]))
-    cantilever_dimensions = np.array([60, 40, 40])
-    cantilever_elements = np.array([1, 1, 1])
+    #data = np.array(([[54.127, 0.724, -11.211], [54.127, 39.276, -11.211], [64.432, -0.669, 27.737], [64.432, 40.669, 27.737]]))
+    #cantilever_dimensions = np.array([60, 40, 40])
+    #cantilever_elements = np.array([1, 1, 1])
 
 
-    cantilever_initial_parameter = np.array([1.0])
+    #cantilever_initial_parameter = np.array([1.0])
 
-    ps = ParameterEstimation()
-    ps.simulation = CantileverSimulation()
-    ps.initial_parameters = cantilever_initial_parameter
-    ps.simulation.set_projection_data(data)
-    ps.simulation.set_cantilever_dimensions(cantilever_dimensions)
-    ps.simulation.set_cantilever_elements(cantilever_elements)
-    ps.simulation.set_diagnostic_level(0)
-    ps.simulation.setup_cantilever_simulation()
-    ps.simulation.prepare_projection()
-    simulation_tuple = (ps.simulation,)
-    ps.set_objective_function(cantilever_objective_function, simulation_tuple)
-    ps.optimise()
-    print ps.solutions.x
+    #ps = ParameterEstimation()
+    #ps.simulation = CantileverSimulation()
+    #ps.initial_parameters = cantilever_initial_parameter
+    #ps.simulation.set_projection_data(data)
+    #ps.simulation.set_cantilever_dimensions(cantilever_dimensions)
+    #ps.simulation.set_cantilever_elements(cantilever_elements)
+    #ps.simulation.set_diagnostic_level(0)
+    #ps.simulation.setup_cantilever_simulation()
+    #ps.simulation.prepare_projection()
+    #simulation_tuple = (ps.simulation,)
+    #ps.set_objective_function(cantilever_objective_function, simulation_tuple)
+    #ps.optimise()
+    #print ps.solutions.x
 
     #print  ps.objective_function(cantilever_initial_parameter, ps.simulation)
 
-    [H, detH, condH, detH0] = ps.evaluate_hessian(ps.solutions.x, 1e-5)
-    print detH
+    #[H, detH, condH, detH0] = ps.evaluate_hessian(ps.solutions.x, 1e-5)
+    #print detH
+
+    cantilever_dimensions = np.array([30, 12, 12])
+    cantilever_elements = np.array([2, 2, 2])
+    true_parameter = np.array([2.1])
+    guess_parameter = np.array([0.8])
+    gravity_vector = np.array([0.0, 0.0, -9.81])
+
+    ps = ParameterEstimation()
+    ps.simulation = CantileverSimulation()
+    ps.simulation.set_cantilever_dimensions(cantilever_dimensions)
+    ps.simulation.set_cantilever_elements(cantilever_elements)
+    ps.simulation.set_gravity_vector(gravity_vector)
+    ps.simulation.set_diagnostic_level(0)
+    ps.simulation.setup_cantilever_simulation()
+    ps.simulation.set_Mooney_Rivlin_parameter_values(true_parameter)
+    ps.simulation.solve_simulation()
+    data = ps.simulation.generate_data(3)
+    ps.simulation.set_projection_data(data)
+    ps.simulation.prepare_projection()
+
+    from CantileverSimulation import destroy_routine
+    destroy_routine(ps.simulation)
+    ps.initial_parameters = guess_parameter
+    ps.simulation.set_projection_data(data)
+    ps.simulation.setup_cantilever_simulation()
+    ps.simulation.prepare_projection()
+    simulation_tuple = (ps.simulation, )
+    ps.set_objective_function(cantilever_objective_function, simulation_tuple)
+    ps.optimise()
+    [HMatrix, detHMatrix, condHMatrix, detH0Matrix] = ps.evaluate_hessian(ps.solutions.x, 1e-7)
+
+    print detHMatrix

sensitivity_study_script.py
@@ -30,7 +30,8 @@ designVariableTwoStep = 45
 angleOneCounter = 0
 angleTwoCounter = 0
 
-HMatrix = detHMatrix = condHMatrix = detH0Matrix = np.zeros((((designVariableOneFinish-designVariableOneStart)/designVariableOneStep) + 1, ((designVariableTwoFinish-designVariableTwoStart)/designVariableTwoStep) + 1))
+detHMatrix = np.zeros((((designVariableOneFinish-designVariableOneStart)/designVariableOneStep) + 1, ((designVariableTwoFinish-designVariableTwoStart)/designVariableTwoStep) + 1))
+#HMatrix = condHMatrix = detH0Matrix = np.zeros((((designVariableOneFinish-designVariableOneStart)/designVariableOneStep) + 1, ((designVariableTwoFinish-designVariableTwoStart)/designVariableTwoStep) + 1))
 
 for i in range(designVariableOneStart, designVariableOneFinish+1, designVariableOneStep):
     for j in range(designVariableTwoStart, designVariableTwoFinish+1, designVariableTwoStep):
@@ -47,21 +48,21 @@ for i in range(designVariableOneStart, designVariableOneFinish+1, designVariable
         ps.simulation.set_projection_data(data)
         ps.simulation.prepare_projection()
 
-        # Now that the data points have been obtained, use them to find a set of optimised parameters.
         destroy_routine(ps.simulation)
         ps.initial_parameters = guess_parameter
         ps.simulation.set_projection_data(data)
         ps.simulation.setup_cantilever_simulation()
         ps.simulation.prepare_projection()
-        simulation_tuple = (ps.simulation,)
+        simulation_tuple = (ps.simulation, )
         ps.set_objective_function(cantilever_objective_function, simulation_tuple)
         try:
             ps.optimise()
         except CMFEError:
-            [HMatrix[angleOneCounter,angleTwoCounter], detHMatrix[angleOneCounter,angleTwoCounter], condHMatrix[angleOneCounter,angleTwoCounter], detH0Matrix[angleOneCounter,angleTwoCounter]] = [float('nan'), float('nan'), float('nan'), float('nan')]
+            [H, detH, condH, detH0] = [float('nan'), float('nan'), float('nan'), float('nan')]
         else:
-            # Now use those optimised parameters to calculate the Hessian metrics.
-            [HMatrix[angleOneCounter,angleTwoCounter], detHMatrix[angleOneCounter,angleTwoCounter], condHMatrix[angleOneCounter,angleTwoCounter], detH0Matrix[angleOneCounter,angleTwoCounter]] = ps.evaluate_hessian(ps.solutions.x, 1e-7)
+            [H, detH, condH, detH0] = ps.evaluate_hessian(ps.solutions.x, 1e-7)
+
+        detHMatrix[angleOneCounter, angleTwoCounter] = detH
 
         destroy_routine(ps.simulation)
         angleTwoCounter += 1