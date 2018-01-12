7b3909317fa5f446b1a0288a8a9e8e76fedf5ead
Jack Adams <jada201@aucklanduni.ac.nz>
2018-01-12 16:10:29 +1300

    Finished mesh convergence

--- unit_tests.py
+++ unit_tests.py
@@ -1,18 +1,18 @@
 import unittest
 import numpy as np
-import cantilever_simulation
+from cantilever_simulation import CantileverSimulation
 from cantilever_simulation import cantilever_objective_function
 
-class CantileverBoundaryConditions(unittest.TestCase):
+class TestCantileverSimulation(unittest.TestCase):
 
     def test_boundary_elements(self):
         """
         Tests that the Cantilever simulation is creating the boundary conditions of a simple cube as expected.
         """
 
-        sim = cantilever_simulation()
-        sim.set_cantilever_dimensions(np.array([60, 40, 40]))
-        sim.set_cantilever_elements(np.array([1, 1, 1]))
+        sim = CantileverSimulation()
+        sim.set_cantilever_dimensions(np.array([30, 12, 12]))
+        sim.set_cantilever_elements(np.array([8, 6, 8]))
         sim.setup_cantilever_simulation()
 
         # Now test the four nodes on the y-face of the element and check that they are all under the fixed condition.
@@ -21,33 +21,3 @@ class CantileverBoundaryConditions(unittest.TestCase):
         #self.assertTrue(function_which_gets_BC_type of node(5) == 'FIXED')
         #self.assertTrue(function_which_gets_BC_type of node(7) == 'FIXED')
 
-
-class CantileverDataGeneration(unittest.TestCase):
-
-    def test_data_generation(self):
-        """
-        Tests that the four corners of the generated data are located at the corners when there is only one element in
-        the FE model.
-        """
-
-        sim = cantilever_simulation()
-        sim.set_cantilever_dimensions(np.array([60, 40, 40]))
-        sim.set_cantilever_elements(np.array([1, 1, 1]))
-        sim.set_gravity_vector(np.array([0.0, 0.0, 0.0]))
-        sim.setup_cantilever_simulation()
-        sim.prepare_projection()
-        cantilever_objective_function(np.array([2.0, 0.0]), sim)
-        dataLocations = sim.generate_data(3)
-
-        self.assertTrue(dataLocations[0] == [60,0,0])
-        self.assertTrue(dataLocations[1] == [60,40,0])
-        self.assertTrue(dataLocations[2] == [60,0,40])
-        self.assertTrue(dataLocations[3] == [60,40,40])
-
-
-###########
-# TESTING #
-###########
-
-dataTest = CantileverDataGeneration()
-dataTest.test_data_generation()
