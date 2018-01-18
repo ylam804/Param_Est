Fixing Hessian calculation error where simulations don't iterate through solver as expected.

output_1/testing_FE_evaluation_1.py
@@ -2,6 +2,7 @@ from parameter_optimisation import ParameterEstimation
 from cantilever_simulation import CantileverSimulation
 from cantilever_simulation import single_layer_objective_function
 from cantilever_simulation import two_layer_objective_function
+from new_evaluate_hessian import new_evaluate_hessian_method
 import numpy as np
 import math
 
@@ -26,7 +27,7 @@ ps = ParameterEstimation()
 ps.simulation = CantileverSimulation()
 ps.simulation.set_cantilever_dimensions(dimensions)
 ps.simulation.set_cantilever_elements(elements)
-ps.simulation.set_gravity_vector(np.array([0.0, 5.0, 5.0]))       #ps.simulation.gravity_vector_calculation(theta, phi))
+ps.simulation.set_gravity_vector(np.array([0.0, 0.0, 10.0]))       #ps.simulation.gravity_vector_calculation(theta, phi))
 ps.simulation.set_diagnostic_level(1)
 ps.simulation.setup_cantilever_simulation()
 ps.simulation.set_Neo_Hookean_single_layer(initial_parameter)
@@ -35,7 +36,7 @@ ps.simulation.solve_simulation()
 #ps.simulation.export_results("output_1/Cantilever")
 ps.simulation.set_projection_data()
 ps.set_objective_function(single_layer_objective_function)
-[H, detH, condH, detH0] = ps.evaluate_hessian(initial_parameter, 1e-7)
+[H, detH, condH, detH0] = ps.new_evaluate_hessian_method(initial_parameter, 1e-7)
 
 print "For angles Theta = {0}, Phi = {1}".format(theta, phi)
 print "Gravity Vector = {0}".format(ps.simulation.gravity_vector)